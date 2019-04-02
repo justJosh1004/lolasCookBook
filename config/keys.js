ea85919a0744870d638b234ed060b38f2e5ea258
justJosh1004 <j.joson@live.com>
2019-04-02 14:17:33 -0400

    secured keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://josh:livefr3e@cluster0-ringu.mongodb.net/recipes?retryWrites=true',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
