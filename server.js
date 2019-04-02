649d6a41aad4052d3115726165c4b4ed4c9e8d9b
justJosh1004 <j.joson@live.com>
2019-04-02 14:49:50 -0400

    Prepared for deploy

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ mongoose
 // Use Routes
 app.use('/api/recipes', recipes);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('lolas-recipes/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'lolas-recipe', 'build', 'index.html')
+    );
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
