fc729e5ded04b284f2b6e6bc9ac382eb7799fb5e
justJosh1004 <j.joson@live.com>
2019-04-08 13:52:17 -0400

    added ingredients to create recipe

--- lolas-recipes/src/components/admin/RecipeForm.js
+++ lolas-recipes/src/components/admin/RecipeForm.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Form, Button, Message, Divider } from 'semantic-ui-react';
+import { Field, FieldArray, reduxForm } from 'redux-form';
+import { Form, Button, Message, Divider, List } from 'semantic-ui-react';
 import { InputField } from 'react-semantic-redux-form';
 
 class RecipeForm extends Component {
+  state = {
+    ingredients: [{ name: 'Chicken' }]
+  };
+
   renderError = ({ error, touched }) => {
     if (touched && error) {
       return <Message error content={error} />;
@@ -33,21 +37,78 @@ class RecipeForm extends Component {
   onSubmit = formValues => {
     this.props.onSubmit(formValues);
   };
+
+  onAddIndgredientsClick = () => {
+    this.setState({ ...this.state });
+    console.log('Adding Ingredient');
+    console.log(this.state);
+  };
+
+  renderAddIngredients = ({ fields }) => (
+    <List>
+      <List.Item>
+        <Button
+          onClick={() => fields.push({})}
+          type="button"
+          fluid
+          color="orange">
+          Add Ingredient
+        </Button>
+      </List.Item>
+      {fields.map((ingredient, index) => (
+        <List.Item key={index}>
+          <List.Content>
+            <Button
+              type="button"
+              onClick={() => fields.remove(index)}
+              floated="right"
+              color="red">
+              Remove Ingredient
+            </Button>
+            <List.Header>Ingredient #{index + 1}</List.Header>
+            <Field
+              name={`${ingredient}.ing`}
+              component={InputField}
+              label="Ingredient Name"
+              placeholder="What is the ingredient?"
+              required
+            />
+            <Form.Group>
+              <Field
+                name={`${ingredient}.quantity`}
+                component={InputField}
+                label="Quantity"
+                placeholder="How much?"
+              />
+              <Field
+                name={`${ingredient}.measurement`}
+                component={InputField}
+                label="Measurement"
+                placeholder="cups, lbs, kilos, tbps, etc."
+              />
+            </Form.Group>
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  );
+
   render() {
+    console.log(this.props
+      );
     return (
       <Form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
         className="recipe-form">
         <Field
           component={InputField}
-          name="recipeName"
+          name="name"
           label="Name of the Recipe"
           placeholder="What is the name of this recipe?"
           required
         />
-        <Button type="button" fluid color="orange">
-          Add Ingredient
-        </Button>
+        <FieldArray name="ingredients" component={this.renderAddIngredients} />
+
         <Divider />
         {/* Redirect back to admin page once done with the form */}
         <Button onClick={this.onSubmit} floated="right" color="olive">
